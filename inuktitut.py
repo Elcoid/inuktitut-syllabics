77b34feff11bb6fb1766cbba9f1fe01cec913a64
Elcoid <47006046+Elcoid@users.noreply.github.com>
2019-01-24 13:09:56 -0500

    Update inuktitut.py
    Updated the info at the beginning

--- inuktitut.py
+++ inuktitut.py
@@ -2,7 +2,7 @@
 
 # Inuktitut Syllabics, version 1
 # This simple Python app is to practice your knowledge of the qaniujaaqpait
-# (Inuktitut syllabary). It is possible to translate from qaniujaaqpait
+# (Inuktitut syllabary). It is possible to transliterate from qaniujaaqpait
 # (Inuktitut syllabary) to qaliujaaqpait (Latin letters) and vice versa, each
 # direction with three difficulty levels. The only difference between the levels
 # is the number of buttons to choose from. The number of correct and wrong
@@ -10,7 +10,7 @@
 
 # Syllabiques inuktitut, version 1
 # Cette application simple en Python est pour pratiquer votre connaissance des
-# qaniujaaqpait (syllabiques inuktitut). Il est possible de traduire des
+# qaniujaaqpait (syllabiques inuktitut). Il est possible de translitérer des
 # qaniujaaqpait (syllabiques inuktitut) vers les qaliujaaqpait (lettres latines)
 # et vice versa, chaque direction avec trois niveaux de difficulté. La seule
 # différence entre les niveaux est le nombre de boutons de sélection. Le
@@ -21,11 +21,10 @@
 # Diese einfache Python-App hat die Funktion Ihr Wissen über das Qaniujaaqpait
 # (Inuktitut Silbe) zu vertiefen. Es ist möglich von das Qaniujaaqpait
 # (Inuktitut Silbe) in das Qaliujaaqpait (lateinische Buchstaben) sowie
-# umgekehrt in jede Richtung zu jeweils drei Schwierigkeitsgraden zu übersetzen.
-# Der einzige Unterschied zwischen den Graden ist die Anzahl der
-# Auswahlschaltflächen. Die Anzahl der richtigen und falschen Antworten
+# umgekehrt in jede Richtung zu jeweils drei Schwierigkeitsgraden zu
+# transliterieren. Der einzige Unterschied zwischen den Graden ist die Anzahl
+# der Auswahlschaltflächen. Die Anzahl der richtigen und falschen Antworten
 # werden auf dem Bildschirm angezeigt, aber sie werden nicht gespeichert.
-
 
 # Some easily tweakable parameters
 generalFontSize = 20 # Font size of the text, except the question display
